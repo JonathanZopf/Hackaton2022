9ec26976f434106ace26660ec9ef03752cde344d
Lukas Karras <karras.lukas@web.de>
2022-10-21 20:35:37 +0200

    initial setup

--- Hackathon/Program.cs
+++ Hackathon/Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Siemens.Simatic.Simulation.Runtime;
 
 namespace Hackathon
 {
     class Program
     {
+        public static IInstance instance { get; set; }
+
         static void Main(string[] args)
         {
+            instance = SimulationRuntimeManager.CreateInterface("002"); //hooks into existing VirtualPLC, works
+
+            instance.CommunicationInterface = ECommunicationInterface.TCPIP;
+            instance.IsSendSyncEventInDefaultModeEnabled = true;
+
         }
     }
 }
